77d5a39c9aedcecb8600c6adca7235d7e40f7b35
Vi <71043551+Vizereth@users.noreply.github.com>
2025-09-27 19:52:13 +0800

    add: games menu list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   // List of routes with labels
-  const routes = [{ path: "/pacman", name: "Play Pac-Man" }];
+  const routes = [{ path: "/pacman", name: "Pac-Man" }];
 </script>
 
 <main class="home">
-  <ul>
+  <ul class="home__list bitcount-single-ink-500">
     {#each routes as route}
-      <li><a href={route.path}>{route.name}</a></li>
+      <li class="home__list-item">
+        <a class="home__list-link" href={route.path}>{route.name}</a>
+      </li>
     {/each}
   </ul>
 </main>
@@ -16,6 +18,21 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 10px;
+
+    &__list {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+    }
+
+    &__list-item {
+      font-size: 2rem;
+      text-transform: uppercase;
+      transition: 0.15s ease-in;
+
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
   }
 </style>
